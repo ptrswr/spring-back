1f4969212b978b9372c9aba48ac9823b130b025d
ptrswr <pswirkaitis@wp.pl>
2021-04-11 23:22:14 +0200

    admin fix

--- src/main/webapp/app/admin/user-management/user-management.service.ts
+++ src/main/webapp/app/admin/user-management/user-management.service.ts
@@ -3,23 +3,23 @@ import buildPaginationQueryOpts from '@/shared/sort/sorts';
 
 export default class UserManagementService {
   public get(userId: number): Promise<any> {
-    return axios.get(`api/admin/users/${userId}`);
+    return axios.get(`api/users/${userId}`);
   }
 
   public create(user): Promise<any> {
-    return axios.post('api/admin/users', user);
+    return axios.post('api/users', user);
   }
 
   public update(user): Promise<any> {
-    return axios.put('api/admin/users', user);
+    return axios.put('api/users', user);
   }
 
   public remove(userId: number): Promise<any> {
-    return axios.delete(`api/admin/users/${userId}`);
+    return axios.delete(`api/users/${userId}`);
   }
 
   public retrieve(req?: any): Promise<any> {
-    return axios.get(`api/admin/users?${buildPaginationQueryOpts(req)}`);
+    return axios.get(`api/users?${buildPaginationQueryOpts(req)}`);
   }
 
   public retrieveAuthorities(): Promise<any> {
